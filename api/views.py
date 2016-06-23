b5fa12f05a3c3567479bfa4437364a47e0c4ac3a
Justin <ginka13@gmail.com>
2016-06-23 14:36:57 -0700

    Added reading events from a location based on facebook API

--- api/views.py
+++ api/views.py
@@ -1,17 +1,24 @@
 from django.shortcuts import render
+import requests
 from allauth.socialaccount.models import SocialToken
+from allauth.socialaccount.models import SocialAppManager
 
 def index(request):
     # template = loader.get_template('templates/test.html')
+    print(request.user.is_authenticated())
+    if request.user.is_authenticated():
+        token = SocialToken.objects.filter(account__user=request.user, account__provider='facebook')
+        print(token[0])
+        json_token = {'access_token': token[0]}
+        #request_string = 'https://graph.facebook.com/search?q=portland&type=event&access_token=' + str(token[0])
+        r = requests.get('https://graph.facebook.com/search?q=portland&type=event',params=json_token)
+        #r = requests.get(request_string)
 
-    # request.user gives us the access 
-    token = SocialToken.objects.filter(account__user=request.user, account__provider='facebook')
-    if request.method == "POST":
-    	print("post")
-    	HttpRequest.POST('graph.facebook.com/?ids=platform,me')
-    elif request.method == "GET":
-    	print("get")
-    	HttpRequest.GET('graph.facebook.com/?ids=platform,me')
-    #print (request.GET['code'])
-    print(token[0])
+        print(r.content)
+        
+        #print (request.GET['code'])
+        #print(token[0])
+
+            
     return render(request, 'api/test.html')
+
