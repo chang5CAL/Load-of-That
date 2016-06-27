87a10862fb359d07e6a19bc74b2f90ad8aa1442e
chang47 <coralbue@gmail.com>
2016-06-27 12:50:22 -0700

    added new bootstrap

--- assets/js/components/Container.jsx
+++ assets/js/components/Container.jsx
@@ -3,6 +3,7 @@ var Header = require('./Header.jsx')
 var LeftCol = require('./LeftCol.jsx')
 var RightCol = require('./RightCol.jsx')
 
+
 var Container = React.createClass ({
 	getInitialState: function() {
 		return {
