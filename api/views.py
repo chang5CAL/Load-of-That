dfd8cde5fcf7c0fa4c124ffe6af2e3df4fd504de
Justin <ginka13@gmail.com>
2016-07-02 10:58:28 -0700

    Adjusted eventbrite code

--- api/views.py
+++ api/views.py
@@ -140,14 +140,16 @@ def meetup(request):
 	#print(r.content)
 	obj = r.json()
 	print(obj['meta'].keys())
-	print(obj['meta']['title'])
+	print(obj['meta']['description'])
 	
 
 	for index in obj['meta']:
             print("Looking!")
             if('title' in index):
+                print("inserting!!")
                 event_info['name'] = index['title']
-            event_info['source'] = 'Meetup'
+                event_info['source'] = 'Meetup'
+                #I can't seem to get the same datas. Does Meetup even have those?
             """if (datetime.now() < datetime.strptime(index['start_time'][:-5], "%Y-%m-%dT%H:%M:%S") and
                 'location' in index['place'] and
                 'city' in index['place']['location'] and
@@ -182,43 +184,43 @@ def eventbrite(request):
 	return HttpResponseRedirect("/")
 
 def eventbrite_call(request):
-	print(request.session.get('eventbrite_time'))
-	if (request.session.get('eventbrite_time')):
-		time = request.session['eventbrite_time']
-		dt = datetime.strptime(time[:-7], "%Y-%m-%d %H:%M:%S")
-		dt = dt + timedelta(hours=1)
-		if (datetime.now() < dt):
-			# access token still alive, make the api call
-			access_token = request.session['eventbrite_access_token']
-			print (access_token)
-			payload = {
-				'token': access_token,
-				'q': "coding",
-				'venue.city': "seattle",
-				'venue.region': 'WA',
-			}
-			r = requests.get('https://www.eventbriteapi.com/v3/events/search/', params=payload)
-			print (r.content)
-                        #obj = json.loads(r.content)
-			for index in r.content:
-                            print("Looking!")
-                            if (datetime.now() < datetime.strptime(index['start_time'][:-5], "%Y-%m-%dT%H:%M:%S") and
-                                'location' in index['place'] and
-                                'city' in index['place']['location'] and
-                                'street' in index['place']['location']):
-                                print("Inserting!")
-                                event_info['name'] = index['name']
-                                event_info['description'] = index['description']
-                                event_info['start_time'] = index['start_time']
-                                event_info['place'] = dict()
-                                event_info['place']['state'] = index['place']['location']['state']
-                                event_info['place']['city'] = index['place']['location']['city']
-                                event_info['place']['street'] = index['place']['location']['street']
-                                event_info['source'] = 'Eventbrite'
-		else:
-			print("deleted sad face")
-			# access token gone, delete it
-			del request.session['eventbrite_time']
-			del request.session['eventbrite_access_token']
+    print(request.session.get('eventbrite_time'))
+    if (request.session.get('eventbrite_time')):
+    	time = request.session['eventbrite_time']
+    	dt = datetime.strptime(time[:-7], "%Y-%m-%d %H:%M:%S")
+    	dt = dt + timedelta(hours=1)
+    	if (datetime.now() < dt):
+            # access token still alive, make the api call
+            access_token = request.session['eventbrite_access_token']
+            print (access_token)
+            payload = {
+    	    	'token': access_token,
+    	    	'q': "coding",
+    	    	'venue.city': "seattle",
+	    	'venue.region': 'WA',
+    	    }
+            r = requests.get('https://www.eventbriteapi.com/v3/events/search/', params=payload)
+            print (r.content)
+            #obj = json.loads(r.content)
+            """for index in r.content:
+                print("Looking!")
+                if (datetime.now() < datetime.strptime(index['start_time'][:-5], "%Y-%m-%dT%H:%M:%S") and
+                    'location' in index['place'] and
+                    'city' in index['place']['location'] and
+                    'street' in index['place']['location']):
+                    print("Inserting!")
+                    event_info['name'] = index['name']
+                    event_info['description'] = index['description']
+                    event_info['start_time'] = index['start_time']
+                    event_info['place'] = dict()
+                    event_info['place']['state'] = index['place']['location']['state']
+                    event_info['place']['city'] = index['place']['location']['city']
+                    event_info['place']['street'] = index['place']['location']['street']
+                    event_info['source'] = 'Eventbrite'"""
+    else:
+        print("deleted sad face")
+        # access token gone, delete it
+        del request.session['eventbrite_time']
+        del request.session['eventbrite_access_token']
 
-	return render(request, 'api/test.html')
+    return render(request, 'api/test.html')
