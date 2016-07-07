36da2876dc88dabc7c558e2f732ce6970ef605bf
Justin <ginka13@gmail.com>
2016-07-07 15:17:57 -0700

    Started creating test function for api testing

--- api/tests.py
+++ api/tests.py
@@ -1,3 +1,7 @@
 from django.test import TestCase
+from django.contrib.auth.models import AnonymousUser, User
+from django.test import TestCase, RequestFactory
 
 # Create your tests here.
+class APIResponseTest():
+	def APITest():
