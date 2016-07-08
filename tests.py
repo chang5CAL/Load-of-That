b8faa63d461c58d6bc6f93ba6b2562e49163517d
Justin <ginka13@gmail.com>
2016-07-08 16:49:06 -0700

    Made a test for each api

--- tests.py
+++ tests.py
@@ -1,18 +1,26 @@
 from django.test import TestCase
 from django.contrib.auth.models import AnonymousUser, User
 from django.test import TestCase, RequestFactory
+from api import views
 import unittest
 
 # Create your tests here.
 class APIResponseTest(unittest.TestCase):
-	def test_NOT(self):
-		self.assertFalse(False)
-	def test(self):
-		self.assertTrue(True)
-	def testagain(self):
-		self.assertTrue(True)
-	def testNew(self):
-		self.assertFalse(False)
+	def test_Facebook(self):
+		if(views.index("city=seattle&type=coding&state=WA&country=US") != HttpResponse(status=404)):
+			self.assertTrue(True)
+		else:
+			self.assertTrue(False)
+	def test_Meetup(self):
+		if(views.Meetup("city=seattle&type=coding&state=WA&country=US") != HttpResponse(status=404)):
+			self.assertTrue(True)
+		else:
+			self.assertTrue(False)
+	def test_EventBrite(self):
+		if(views.EventBrite_Call("city=seattle&type=coding&state=WA&country=US") != HttpResponse(status=404)):
+			self.assertTrue(True)
+		else:
+			self.assertTrue(False)
 
 if __name__ == '__main__':
     unittest.main()
