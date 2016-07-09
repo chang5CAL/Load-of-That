bd853dcdb2be969a6cf05d5a9aef7b1ccafb42da
Justin <ginka13@gmail.com>
2016-07-09 09:31:34 -0700

    Made tests with requests instead of json objects

--- tests.py
+++ tests.py
@@ -3,21 +3,27 @@ from django.contrib.auth.models import AnonymousUser, User
 from django.test import TestCase, RequestFactory
 from api import views
 import unittest
+import json
+from django.http import HttpRequest
 
 # Create your tests here.
 class APIResponseTest(unittest.TestCase):
 	def test_Facebook(self):
-		if(views.index("city=seattle&type=coding&state=WA&country=US") != HttpResponse(status=404)):
+		req = HttpRequest("city=seattle&type=coding&state=WA&country=US")
+		if(views.index(req) != HttpResponse(status=404)):
 			self.assertTrue(True)
 		else:
+			print("Test")
 			self.assertTrue(False)
 	def test_Meetup(self):
-		if(views.Meetup("city=seattle&type=coding&state=WA&country=US") != HttpResponse(status=404)):
+		req = HttpResponse("city" : "seattle", "type" : "coding", "state" : "WA", "country":"US")
+		if(views.meetup(jsreqonObj) != HttpResponse(status=404)):
 			self.assertTrue(True)
 		else:
 			self.assertTrue(False)
 	def test_EventBrite(self):
-		if(views.EventBrite_Call("city=seattle&type=coding&state=WA&country=US") != HttpResponse(status=404)):
+		req = HttpRequest("city=seattle&type=coding&state=WA&country=US")
+		if(views.eventbrite_call(req) != HttpResponse(status=404)):
 			self.assertTrue(True)
 		else:
 			self.assertTrue(False)
